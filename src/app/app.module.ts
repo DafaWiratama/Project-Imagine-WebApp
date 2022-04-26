7b77c432a48d2c2416a3d7af4cab2d634dbb3a1b
dafaw <dafawiratama13@gmail.com>
2022-04-26 15:15:09 +0700

    Base Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import 'img-comparison-slider';
+import { UpscalerComponent } from './landing/upscaler/upscaler.component';
+import { BannerComponent } from './landing/banner/banner.component';
+import { ArticleComponent } from './landing/article/article.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSelectModule} from "@angular/material/select";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UpscalerComponent,
+    BannerComponent,
+    ArticleComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    MatSelectModule,
+    NoopAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule { }
